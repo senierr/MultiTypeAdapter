63a05f9c83f7b5e0024218c498c085b3ce1d7b97
senierr <senierr_zhou@163.com>
2017-11-01 19:13:07 +0800

    更新通用分割线

--- library/src/main/java/com/senierr/adapter/support/RVItemDecoration.java
+++ library/src/main/java/com/senierr/adapter/support/RVItemDecoration.java
@@ -36,22 +36,65 @@ public class RVItemDecoration extends RecyclerView.ItemDecoration {
         final int currentPosition = parent.getChildLayoutPosition(view);
         if (currentPosition == -1) return;
 
-        outRect.set(0, 0, dividerSize, dividerSize);
-
         SpanState spanState = getSpanState(view, parent, state);
+        int eachSize = (spanState.spanCount - 1) * dividerSize / spanState.spanCount;
         if (orientation == OrientationHelper.VERTICAL) {
-            if (spanState.isLastSpanGroup) {
+            // top, bottom
+            if (spanState.isFirstSpanGroup && spanState.isLastSpanGroup) {
+                outRect.top = 0;
                 outRect.bottom = 0;
+            } else if (spanState.isFirstSpanGroup) {
+                outRect.top = 0;
+                outRect.bottom = eachSize;
+            } else if (spanState.isLastSpanGroup) {
+                outRect.top = eachSize;
+                outRect.bottom = 0;
+            } else {
+                outRect.top = eachSize / 2;
+                outRect.bottom = eachSize / 2;
             }
-            if (spanState.isEndSpan) {
+            // left, right
+            if (spanState.isStartSpan && spanState.isEndSpan) {
+                outRect.left = 0;
                 outRect.right = 0;
+            } else if (spanState.isStartSpan) {
+                outRect.left = 0;
+                outRect.right = eachSize;
+            } else if (spanState.isEndSpan) {
+                outRect.left = eachSize;
+                outRect.right = 0;
+            } else {
+                outRect.left = eachSize / 2;
+                outRect.right = eachSize / 2;
             }
         } else {
-            if (spanState.isLastSpanGroup) {
+            // left, right
+            if (spanState.isFirstSpanGroup && spanState.isLastSpanGroup) {
+                outRect.left = 0;
                 outRect.right = 0;
+            } else if (spanState.isFirstSpanGroup) {
+                outRect.left = 0;
+                outRect.right = eachSize;
+            } else if (spanState.isLastSpanGroup) {
+                outRect.left = eachSize;
+                outRect.right = 0;
+            } else {
+                outRect.left = eachSize / 2;
+                outRect.right = eachSize / 2;
             }
-            if (spanState.isEndSpan) {
+            // top, bottom
+            if (spanState.isStartSpan && spanState.isEndSpan) {
+                outRect.top = 0;
                 outRect.bottom = 0;
+            } else if (spanState.isStartSpan) {
+                outRect.top = 0;
+                outRect.bottom = eachSize;
+            } else if (spanState.isEndSpan) {
+                outRect.top = eachSize;
+                outRect.bottom = 0;
+            } else {
+                outRect.top = eachSize / 2;
+                outRect.bottom = eachSize / 2;
             }
         }
     }
@@ -67,17 +110,39 @@ public class RVItemDecoration extends RecyclerView.ItemDecoration {
             final View child = parent.getChildAt(i);
             parent.getDecoratedBoundsWithMargins(child, childBounds);
             SpanState spanState = getSpanState(child, parent, state);
-
+            int eachSize = (spanState.spanCount - 1) * dividerSize / spanState.spanCount;
             if (orientation == OrientationHelper.VERTICAL) {
-                // 画垂直分割线
-                if (!spanState.isEndSpan) {
-                    dividerDrawable.setBounds(childBounds.right - dividerSize, childBounds.top,
+                // top, bottom
+                if (!spanState.isFirstSpanGroup && !spanState.isLastSpanGroup) {
+                    dividerDrawable.setBounds(childBounds.left, childBounds.top,
+                            childBounds.right, childBounds.top +  eachSize / 2);
+                    dividerDrawable.draw(c);
+                    dividerDrawable.setBounds(childBounds.left, childBounds.bottom - eachSize / 2,
+                            childBounds.right, childBounds.bottom);
+                    dividerDrawable.draw(c);
+                } else if (!spanState.isFirstSpanGroup) {
+                    dividerDrawable.setBounds(childBounds.left, childBounds.top,
+                            childBounds.right, childBounds.top +  eachSize);
+                    dividerDrawable.draw(c);
+                } else if (!spanState.isLastSpanGroup) {
+                    dividerDrawable.setBounds(childBounds.left, childBounds.bottom - eachSize,
                             childBounds.right, childBounds.bottom);
                     dividerDrawable.draw(c);
                 }
-                // 画水平分割线
-                if (!spanState.isLastSpanGroup) {
-                    dividerDrawable.setBounds(childBounds.left, childBounds.bottom - dividerSize,
+                // left, right
+                if (!spanState.isStartSpan && !spanState.isEndSpan) {
+                    dividerDrawable.setBounds(childBounds.left, childBounds.top,
+                            childBounds.left + eachSize / 2, childBounds.bottom);
+                    dividerDrawable.draw(c);
+                    dividerDrawable.setBounds(childBounds.right - eachSize / 2, childBounds.top,
+                            childBounds.right, childBounds.bottom);
+                    dividerDrawable.draw(c);
+                } else if (!spanState.isStartSpan) {
+                    dividerDrawable.setBounds(childBounds.left, childBounds.top,
+                            childBounds.left + eachSize, childBounds.bottom);
+                    dividerDrawable.draw(c);
+                } else if (!spanState.isEndSpan) {
+                    dividerDrawable.setBounds(childBounds.right - eachSize, childBounds.top,
                             childBounds.right, childBounds.bottom);
                     dividerDrawable.draw(c);
                 }
@@ -91,6 +156,41 @@ public class RVItemDecoration extends RecyclerView.ItemDecoration {
                 // 画水平分割线
                 if (!spanState.isEndSpan) {
                     dividerDrawable.setBounds(childBounds.left, childBounds.bottom - dividerSize,
+                            childBounds.right, childBounds.bottom);
+                    dividerDrawable.draw(c);
+                }
+
+                // left, right
+                if (!spanState.isFirstSpanGroup && !spanState.isLastSpanGroup) {
+                    dividerDrawable.setBounds(childBounds.left, childBounds.top,
+                            childBounds.left + eachSize / 2, childBounds.bottom);
+                    dividerDrawable.draw(c);
+                    dividerDrawable.setBounds(childBounds.right - eachSize / 2, childBounds.top,
+                            childBounds.right, childBounds.bottom);
+                    dividerDrawable.draw(c);
+                } else if (!spanState.isFirstSpanGroup) {
+                    dividerDrawable.setBounds(childBounds.left, childBounds.top,
+                            childBounds.left + eachSize, childBounds.bottom);
+                    dividerDrawable.draw(c);
+                } else if (!spanState.isLastSpanGroup) {
+                    dividerDrawable.setBounds(childBounds.right - eachSize, childBounds.top,
+                            childBounds.right, childBounds.bottom);
+                    dividerDrawable.draw(c);
+                }
+                // top, bottom
+                if (!spanState.isStartSpan && !spanState.isEndSpan) {
+                    dividerDrawable.setBounds(childBounds.left, childBounds.top,
+                            childBounds.right, childBounds.top +  eachSize / 2);
+                    dividerDrawable.draw(c);
+                    dividerDrawable.setBounds(childBounds.left, childBounds.bottom - eachSize / 2,
+                            childBounds.right, childBounds.bottom);
+                    dividerDrawable.draw(c);
+                } else if (!spanState.isStartSpan) {
+                    dividerDrawable.setBounds(childBounds.left, childBounds.top,
+                            childBounds.right, childBounds.top +  eachSize);
+                    dividerDrawable.draw(c);
+                } else if (!spanState.isEndSpan) {
+                    dividerDrawable.setBounds(childBounds.left, childBounds.bottom - eachSize,
                             childBounds.right, childBounds.bottom);
                     dividerDrawable.draw(c);
                 }
@@ -121,19 +221,31 @@ public class RVItemDecoration extends RecyclerView.ItemDecoration {
             int spanSize = gridLayoutManager.getSpanSizeLookup().getSpanSize(currentPosition);
             int spanIndex = gridLayoutManager.getSpanSizeLookup().getSpanIndex(currentPosition, spanCount);
             int spanGroupIndex = gridLayoutManager.getSpanSizeLookup().getSpanGroupIndex(currentPosition, spanCount);
-            // 判断是否是最后一组Span
+
+            spanState.spanCount = spanCount;
+            // 判断是否第一组
+            if (spanGroupIndex == 0) {
+                spanState.isFirstSpanGroup = true;
+            }
+            // 判断是否最后一组
             spanState.isLastSpanGroup = true;
             for (int i = currentPosition + 1; i < currentPosition + 1 + spanCount; i++) {
+                // 判断是否越界
                 if (i >= state.getItemCount()) {
                     break;
                 }
+                // 判断是否还有下一组
                 int groupIndex = gridLayoutManager.getSpanSizeLookup().getSpanGroupIndex(i, spanCount);
                 if (groupIndex > spanGroupIndex) {
                     spanState.isLastSpanGroup = false;
                     break;
                 }
             }
-            // 判断是否是组末Span
+            // 判断是否组始Span
+            if (spanIndex == 0) {
+                spanState.isStartSpan = true;
+            }
+            // 判断是否组末Span
             spanState.isEndSpan = spanIndex + spanSize == spanCount;
         } else if (layoutManager instanceof LinearLayoutManager) {
             LinearLayoutManager linearLayoutManager = (LinearLayoutManager) layoutManager;
@@ -141,7 +253,12 @@ public class RVItemDecoration extends RecyclerView.ItemDecoration {
                 orientation = linearLayoutManager.getOrientation();
             }
 
+            spanState.spanCount = 1;
+            if (currentPosition == 0) {
+                spanState.isFirstSpanGroup = true;
+            }
             spanState.isLastSpanGroup = state.getItemCount() - currentPosition - 1 <= 0;
+            spanState.isStartSpan = true;
             spanState.isEndSpan = true;
         } else if (layoutManager instanceof StaggeredGridLayoutManager) {
             StaggeredGridLayoutManager staggeredGridLayoutManager = (StaggeredGridLayoutManager) layoutManager;
@@ -154,21 +271,29 @@ public class RVItemDecoration extends RecyclerView.ItemDecoration {
             int spanCount = staggeredGridLayoutManager.getSpanCount();
             int spanIndex = layoutParams.getSpanIndex();
 
+            spanState.spanCount = spanCount;
+            spanState.isFirstSpanGroup = true;
             spanState.isLastSpanGroup = layoutParams.isFullSpan() && currentPosition == state.getItemCount() - 1;
+            spanState.isStartSpan = layoutParams.isFullSpan() || (!layoutParams.isFullSpan() && spanIndex == 0);
             spanState.isEndSpan = layoutParams.isFullSpan() || (!layoutParams.isFullSpan() && spanIndex == spanCount - 1);
         }
         return spanState;
     }
 
     private class SpanState {
+        int spanCount;
+        boolean isFirstSpanGroup = false;
         boolean isLastSpanGroup = false;
+        boolean isStartSpan = false;
         boolean isEndSpan = false;
 
         @Override
         public String toString() {
-            return "SpanState{" +
-                    "isLastSpanGroup=" + isLastSpanGroup +
-                    ", isEndSpan=" + isEndSpan +
+            return "SpanState {" +
+                    "isFirstSpanGroup = " + isFirstSpanGroup +
+                    ", isLastSpanGroup = " + isLastSpanGroup +
+                    ", isStartSpan = " + isStartSpan +
+                    ", isEndSpan = " + isEndSpan +
                     '}';
         }
     }
